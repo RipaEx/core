47f9fdbd11bfbb8c97f82475ca0872b57e72c604
Giovanni Silvestri <gsit80@gmail.com>
2019-09-08 17:48:18 +0200

    Core Upgrade - Arch: minimum network reach for MAinNET

--- packages/core/src/config/mainnet/plugins.js
+++ packages/core/src/config/mainnet/plugins.js
@@ -47,6 +47,8 @@ module.exports = {
     "@arkecosystem/core-p2p": {
         host: process.env.CORE_P2P_HOST || "0.0.0.0",
         port: process.env.CORE_P2P_PORT || 5501,
+        minimumNetworkReach: 2,
+        coldStart: 30,        
     },
     "@arkecosystem/core-blockchain": {
         fastRebuild: false,
